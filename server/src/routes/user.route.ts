6412f4d25574931e46ae2385f64d7b0fe4715c3c
Johannes Andersson <hello@thejoltjoker.com>
2024-03-02 13:06:20 +0000

    Remove newlines

--- server/src/routes/user.route.ts
+++ server/src/routes/user.route.ts
@@ -6,31 +6,25 @@ import {
   update as updateFavorite,
 } from "../controllers/favorites.controller";
 import { create, get, remove, update } from "../controllers/user.controller";
-import { ImageItemSchema } from "../schemas/imageItem.schema";
 import { UserFavoriteSchema } from "../schemas/favorite.schema";
+import { ImageItemSchema } from "../schemas/imageItem.schema";
 import { UserSchema } from "../schemas/user.schema";
 import { validate } from "../validate";
 
 const router = express.Router();
 
 router.get("/:userId", get);
-
 router.post("/", validate(UserSchema), create);
-
 router.put("/:userId", validate(UserSchema), update);
-
 router.delete("/:userId", remove);
 
 router.get("/:userId/favorites", getFavorite);
-
 router.post("/:userId/favorites", validate(ImageItemSchema), createFavorite);
-
 router.put(
   "/:userId/favorites/:favoriteId",
   validate(UserFavoriteSchema),
   updateFavorite
 );
-
 router.delete("/:userId/favorites/:favoriteId", removeFavorite);
 
 export default router;
